5279eb5147d461f408b02abd32e5ef01a87dc34d
Tyreese Learmond <tyreeselearmond@Tyreeses-MacBook-Air.local>
2026-01-06 13:19:08 -0600

    Update formula to point to v0.0.0 release

--- learmond.rb
+++ learmond.rb
@@ -1,7 +1,7 @@
 class Learmond < Formula
   desc "Learmond CLI"
   homepage "https://github.com/thelearmondcorporation/learmond"
-  url "https://github.com/thelearmondcorporation/learmond/releases/download/v0.0.1/learmond"
+  url "https://github.com/thelearmondcorporation/learmond/releases/download/v0.0.0/learmond"
   version "0.0.0"
   sha256 'a1a8eb893df3e5a3d954ad9154cbc91196665383bc389f869788c75fb866a2ea'
   license "MIT"
