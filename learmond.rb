f25146515cfcc6e56306064940293ec56cbcae77
Tyreese Learmond <tyreeselearmond@Tyreeses-MacBook-Air.local>
2026-01-24 13:40:56 -0600

    Release v0.1.0, Breaking release.

--- learmond.rb
+++ learmond.rb
@@ -1,9 +1,9 @@
 class Learmond < Formula
   desc "Learmond CLI"
   homepage "https://github.com/thelearmondcorporation/learmond"
-  url "https://github.com/thelearmondcorporation/learmond/releases/download/v0.0.1/learmond"
-  version "0.0.1"
-  sha256 '951c4ee29f3de041b95cda563909c951ae61dbd7b18cc3b85d6a9aa179d3280e'
+  url "https://github.com/thelearmondcorporation/learmond/releases/download/v0.1.0/learmond"
+  version "0.1.0"
+  sha256 '2c0272f870052dedd41e9270f66badf29daa4923b35d4b509d4d3e20c0e5d8ce'
   license "MIT"
 
   def install
